84ebcdbe94a4fe57a13f3ffdff4679b0f7184519
su-mio <yoshi.koshien@gmail.com>
2015-05-16 20:43:23 +0900

    Finish Sample Ensyu

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App!" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 	
 	it "should have the right title" do
 	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	  expect(page).to have_title("#{base_title} | Home")
 	end
   end
 
@@ -23,7 +25,7 @@ describe "StaticPages" do
 	end
 	it "should have the right title" do
 	  visit '/static_pages/help'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	  expect(page).to have_title("#{base_title} | Help")
 	end
   end
 
@@ -36,7 +38,15 @@ describe "StaticPages" do
 
 	it "should have the right title" do
 	  visit '/static_pages/about'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+	  expect(page).to have_title("#{base_title} | About Us")
+	end
+  end
+
+  describe "Contact" do
+
+    it "should have the right title" do
+	  visit '/static_pages/contact'
+	  expect(page).to have_title("#{base_title} | Contact")
 	end
   end
 end
